f6effa40d80f01d6dabe6a63c20eda3f8f1c56a9
Adam Grande <adam.grande@outlook.com>
2019-12-21 12:24:30 +0800

    Moved all prereqs to their own function and cleaned up things a bit

--- installerifier.ps1
+++ installerifier.ps1
@@ -15,28 +15,49 @@ function Missing-Command
     return $result
 }
 
-
-Write-Host "Finding Chocolatey..."
-if(Missing-Command -executable "choco.exe")
+function Install-Prerequesities 
 {
-    Write-Host "Chocolatey not found... installing it now!"
-
-    #Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    #iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-    Write-Host 
-    Write-Host "You need to restart your shell before you continue, otherwise Chocolatey may not be recognised correctly."
-    Write-Host "This is Bad™ becuase you wont be able to run the rest of the script!"
+    Write-Host "Installing pre-requesites for this script."
     Write-Host
-    return
-}
-else 
-{
-    # todo: Look at using Update-SessionEnvironment helper from chocolatey.
-    Write-Host "Chocolatey is present! Continuing with the rest of the script..."
+
+    Write-Host "Installing Nuget Provider..."
+    Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force 
+    Write-Host "NuGet package provider install!"
+
+    Write-Host "Trusting the main PSGallery so we can install things..."
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+    Write-Host "Installing how we detect Visual Studio..."
+    Install-Module VSSetup -Scope CurrentUser
+
+    Write-Host "Finding Chocolatey..."
+    if(Missing-Command -executable "choco.exe")
+    {
+        Write-Host "Chocolatey not found... installing it now!"
+
+        #Set-ExecutionPolicy Bypass -Scope Process -Force; 
+        #iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+        Write-Host 
+        Write-Host "You need to restart your shell before you continue, otherwise Chocolatey may not be recognised correctly."
+        Write-Host "This is Bad™ becuase you wont be able to run the rest of the script!"
+        Write-Host
+        return 1
+    }
+
+    Write-Host "Installing SQL Server modules"
+    Install-Module -Name SqlServer
+
+    Write-Host "Pre-requesities installed!"
     Write-Host
-    Write-Host "Go grab a coffee and come back later <3"
+    # Todo: Install Update-SessionEnvironment
 }
+
+# todo check exit code here
+#Install-Prerequesities
+
+
+
 
 # Git
 if(Missing-Command -executable "git.exe")
@@ -45,7 +66,7 @@ if(Missing-Command -executable "git.exe")
     Write-Host "Git not found."
     Write-Host "Installing..."
 
-    choco install git -y
+    #choco install git -y
 
     Write-Host "Git is installed!"
 }
@@ -61,7 +82,7 @@ if(Missing-Command -executable "code.exe")
     Write-Host "VSCode not found."
     Write-Host "Installing..."
 
-    #choco install vscode
+    #choco install vscode -y
 
     Write-Host "VSCode is installed!"
 }
@@ -70,6 +91,43 @@ else
     Write-Host "VSCode is already installed."
 }
 
+
+$vs2017 = Get-VSSetupInstance -All | Select-VSSetupInstance -Require 'Microsoft.VisualStudio.Workload.NativeDesktop' -Version '[15.0,16.0)' -Latest
+
+if($vs2017 -eq $null) 
+{
+    Write-Host "Visual Studio 2019 not found."
+
+    # lmao this is super heavy handed but the mobile dev package is currently failing a test in chocolately :(
+    choco install visualstudio2019professional --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US" -y
+
+    Write-Host "Visual Studio 2019 is installed!"
+}
+else 
+{
+    Write-Host "Visual Studio 2019 already exists"
+}
+try 
+{
+    # todo make this express specific
+    $sqlExpress = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
+    choco install sql-server-express -y
+}
+catch 
+{
+    Write-Host "SQL Server Express not found"
+}
+
+$sqlInstance = Get-SqlInstance -ServerInstance ".\sqlexpress"
+
+if($sqlInstance -eq $null)
+{
+    choco install sql-server-management-studio
+}
+else 
+{
+    Write-Host "SQL Server Express already installed!"
+}
 
 # Visual Studio, with Desktop, Mobile, and Web Workloads
 # Sql Express
